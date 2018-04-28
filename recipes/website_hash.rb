1bd24109c338a26fdeaf28947492f9ea939e3dfc
Stig Røsnes <androidstigrr@gmail.com>
2018-04-28 12:53:50 +0200

    Ny recipe for behandling av webside hash

---
+++ recipes/website_hash.rb
@@ -0,0 +1,10 @@
+lunch_order = {
+  "03" => "forsent",
+  "06" => "fortidlig",
+  "09" => "morgen",
+  "12" => "lunsj"
+  "15" => "middag",
+  "18" => "ettermiddag",
+  "21" => "senkveld",
+  "24" => "midnatt",
+}

dcd232faf20aaa39dfa99ed52608868d616a94ed
Stig Røsnes <androidstigrr@gmail.com>
2018-04-30 13:50:18 +0200

    more syntax learn, need I

--- recipes/website.rb
+++ recipes/website.rb
@@ -9,35 +9,44 @@ middag  = Time.new(t.year, t.month, t.day, 15, 0, 0, t.utc_offset)
 ettermiddag = Time.new(t.year, t.month, t.day, 18, 0, 0, t.utc_offset)
 senkveld  = Time.new(t.year, t.month, t.day, 21, 0, 0, t.utc_offset)
 
-if t.between?(midnatt, forsent)
-        tweb = source 'wem.htm.erb'
-end
-elsif t.between?(forsent, fortidlig)
-        template '/var/www/html/index.html' do
+if t.between?(midnatt, forsent) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
+        source 'wem.htm.erb'
+        end
+    end
+elsif t.between?(forsent, fortidlig) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wfs.htm.erb'
-end
-elsif t.between?(fortidlig, morgen)
-        template '/var/www/html/index.html' do
+        end
+    end
+elsif t.between?(fortidlig, morgen) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wft.htm.erb'
-end
-elsif t.between?(morgen, lunsj)
-        template '/var/www/html/index.html' do
+        end
+    end
+elsif t.between?(morgen, lunsj) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wmo.htm.erb'
-end
-elsif t.between?(lunsj, middag)
-        template '/var/www/html/index.html' do
+        end
+    end
+elsif t.between?(lunsj, middag) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wlu.htm.erb'
-end
-elsif t.between?(middag, ettermiddag)
-        template '/var/www/html/index.html' do
+        end
+    end
+elsif t.between?(middag, ettermiddag) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wmi.htm.erb'
-end
-elsif t.between?(ettermiddag, senkveld)
-        template '/var/www/html/index.html' do
+        end
+    end
+elsif t.between?(ettermiddag, senkveld) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wet.htm.erb'
-end
-else t.between?(senkveld, midnatt)
-        template '/var/www/html/index.html' do
+        end
+    end
+else t.between?(senkveld, midnatt) do
+    template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wsk.htm.erb'
-end
+        end
+    end
 end

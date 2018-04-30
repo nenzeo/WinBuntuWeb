12b18a020eac8730910400780f8c3ae2101082bb
Stig Røsnes <androidstigrr@gmail.com>
2018-04-30 14:04:57 +0200

    stupid do was at fault

--- recipes/website.rb
+++ recipes/website.rb
@@ -9,44 +9,36 @@ middag  = Time.new(t.year, t.month, t.day, 15, 0, 0, t.utc_offset)
 ettermiddag = Time.new(t.year, t.month, t.day, 18, 0, 0, t.utc_offset)
 senkveld  = Time.new(t.year, t.month, t.day, 21, 0, 0, t.utc_offset)
 
-if t.between?(midnatt, forsent) do
+if t.between?(midnatt, forsent)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wem.htm.erb'
-        end
     end
-elsif t.between?(forsent, fortidlig) do
+elsif t.between?(forsent, fortidlig)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wfs.htm.erb'
-        end
     end
-elsif t.between?(fortidlig, morgen) do
+elsif t.between?(fortidlig, morgen)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wft.htm.erb'
-        end
     end
-elsif t.between?(morgen, lunsj) do
+elsif t.between?(morgen, lunsj)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wmo.htm.erb'
-        end
     end
-elsif t.between?(lunsj, middag) do
+elsif t.between?(lunsj, middag)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wlu.htm.erb'
-        end
     end
-elsif t.between?(middag, ettermiddag) do
+elsif t.between?(middag, ettermiddag)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wmi.htm.erb'
-        end
     end
-elsif t.between?(ettermiddag, senkveld) do
+elsif t.between?(ettermiddag, senkveld)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wet.htm.erb'
-        end
     end
-else t.between?(senkveld, midnatt) do
+else t.between?(senkveld, midnatt)
     template 'c:\\inetpub\\wwwroot\\Default.htm' do
         source 'wsk.htm.erb'
-        end
     end
 end
